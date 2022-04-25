8d5082093aa233e76757adb7bf829fe51ce36cac
Md.Shohagh <96836985+sbsohag3@users.noreply.github.com>
2022-04-26 02:03:41 +0600

    heroku test

--- index.js
+++ index.js
@@ -61,6 +61,11 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/hero", (req, res) => {
+  res.send("heroku test");
+});
+
+
 app.listen(port, () => {
   console.log("Jhon is running op port", port);
 });
